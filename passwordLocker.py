d2820cefcfb02f1b562d4c8e7af5f35953287c33
Krasivaya <semwagacarine@gmail.com>
2019-09-06 11:37:26 +0200

    create an empty object to hold user list

--- passwordLocker.py
+++ passwordLocker.py
@@ -2,9 +2,14 @@ class User:
     '''
     User class that generate new instances of users
     '''
+    user_list = []
+    # Empty object to hold user list
+
     def __init__(self,username,password):
         '''
-        Method to set what to except from the user
+        __init__ Method to define user properties (what to except from the user)
         '''
         self.username = username
         self.password = password
+    #The end!
+
