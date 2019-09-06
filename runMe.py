748a813769c32575dcff617d96776a32c7377a17
Krasivaya <semwagacarine@gmail.com>
2019-09-06 16:37:39 +0200

    Add create user method

--- runMe.py
+++ runMe.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3.6
-from passwordLocker import User, Credentials #Importing user and credentials class
+from passwordLocker import User, Credentials #Importing user and credentials classes
 
+def create_user(username,password):
+    '''
+    create_user method to create a new user
+    '''
+    new_user = User(username,password)
+    return new_user
