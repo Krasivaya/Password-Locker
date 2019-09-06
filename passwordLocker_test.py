f6a97e2518dfa3c997985f28c4fa9c3482ba0b15
Krasivaya <semwagacarine@gmail.com>
2019-09-06 12:20:43 +0200

    Add test_save_user test case

--- passwordLocker_test.py
+++ passwordLocker_test.py
@@ -20,3 +20,13 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.password,'hideme')
     #The End!
     
+    def test_save_user(self):
+        '''
+        test_save_user test case to test if the user object is saved into the user list
+        '''
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
+    #The end!
+
+if __name__ == '__main__':
+    unittest.main()
