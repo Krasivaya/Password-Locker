8199ee3615ecf2a9d242387a740b95ccede49ef9
Krasivaya <semwagacarine@gmail.com>
2019-09-06 11:48:10 +0200

    Add delete method

--- passwordLocker.py
+++ passwordLocker.py
@@ -12,16 +12,27 @@ class User:
         self.username = username
         self.password = password
     #The end!
+
     def save_user(self):
         '''
         save_user method to save a new user to user_list 
         '''
         User.user_list.append(self)
     #The end!
+
     @classmethod
     def display_user(cls):
         '''
         display_user method to display user_list
         '''
         return cls.user_list
+    #The end!
+
+    def delete_user(self):
+        '''
+        delete_user method to delete a user stored in user_list
+        '''
+        User.user_list.remove(self)
+    #The End!
+
 
