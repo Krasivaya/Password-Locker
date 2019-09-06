fda7a83debe6739ba31abd4de401880b03c390b9
Krasivaya <semwagacarine@gmail.com>
2019-09-06 11:30:40 +0200

    init method to define user properties

--- passwordLocker.py
+++ passwordLocker.py
@@ -1,4 +1,10 @@
 class User:
     '''
-    User class that generate a user account
+    User class that generate new instances of users
     '''
+    def __init__(self,username,password):
+        '''
+        Method to set what to except from the user
+        '''
+        self.username = username
+        self.password = password
