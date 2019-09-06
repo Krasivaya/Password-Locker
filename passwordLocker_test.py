529bea2aec9fbf7997468338b6e0f52e5d361b68
Krasivaya <semwagacarine@gmail.com>
2019-09-06 11:55:45 +0200

    Add testUser class

--- passwordLocker_test.py
+++ passwordLocker_test.py
@@ -1,3 +1,9 @@
 import unittest # Importing unittest module
 from passwordLocker import User # Importing user class from passwordLocker module
 
+class TestUser(unittest.TestCase):
+    '''
+    TestUser class to define test cases for the user class behaviours
+    '''
+    
+    
